99391efb342a3cbdd05ebcf4555be0c4f729d15d
Algie <algie@algie-linux16.04>
2018-01-12 17:08:29 +0800

    search people only on fb and fix search result container to scrape

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ async function letsCrawl() {
     console.log('Crawling begin...');
 
     const browser = await puppeteer.launch({
-        headless: false 
+        headless: false //set to false in order to see how the operation goes
     });
     const page = await browser.newPage();
     await loginToFaceBook(page);
-    await searchPerson(page);
+    await searchPerson(page, browser);
 }
 
 /*** INITIATE/SIMULATE LOGIN TO FACEBOOK ***/
@@ -33,49 +33,55 @@ async function loginToFaceBook(page) {
 }
 
 /*** SIMULATE A SEARCH IN FACEBOOK SEARCH ***/
-async function searchPerson(page) {
+async function searchPerson(page, browser) {
     const KEYWORD = 'kevin';
-    const SEARCH_URL = 'https://www.facebook.com/search/top/?q='+KEYWORD;
+    const SEARCH_URL = 'https://www.facebook.com/search/people/?q='+KEYWORD;
     
     console.log('Searching for a person named: '+KEYWORD);
 
     await page.goto(SEARCH_URL);
-    await page.waitFor(3*1000);
+    await page.waitFor(5*1000);
 
+    //take a screenshot
+    takeScreenshot(SEARCH_URL, 'fb-'+KEYWORD);
     // we will scrape/get all the full names from the results
-    scrapeFullNameFromTheResults(page); 
+    scrapeFullNameFromTheResults(page, browser); 
 }
 
 /*** LETS GET ALL RESULTS FULLNAME ***/
-async function scrapeFullNameFromTheResults(page) {
+async function scrapeFullNameFromTheResults(page, browser) {
     // this is the list wrapper selector for each result
-    const RESULT_LIST_CLASS_SELECTOR = "_2yer _401d _2xje _2nuh";
+    const CONTAINER_SELECTOR = "BrowseResultsContainer";
 
     // lets first evaluate and see the search result length
     let resultLength = await page.evaluate((sel) => {
-        return document.getElementsByClassName(sel).length;
-    }, RESULT_LIST_CLASS_SELECTOR);
+        return document.getElementById(sel).childNodes.length;
+    }, CONTAINER_SELECTOR);
     let resultcount = resultLength;
 
     console.log('Search results: '+ resultcount);
-    console.log('*** Getting the fullnames ***');
+    console.log('*** Getting the fullnames from search result ***');
 
     // lets get each result wrapped content
     for (let i = 1; i <= resultLength; i++) {
         let fullname = await page.evaluate((sel) => {
-            return document.querySelector(sel);
-        }, '#initial_browse_result > div#xt_uniq_3');
-        //let fn = document.getElementById("#js_4c").innerHTML;	
+            return document.querySelector(sel).innerHTML;
+	}, '#' +CONTAINER_SELECTOR+ ' > div:nth-child(' +i+ ') > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > a > span');
+
+	// lets display each fullname that we scraped!
 	console.log(i+'. '+fullname);
     }
+
+    //lets close the browser
+    browser.close();
 }
 
 async function takeScreenshot(url, filename) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.screenshot({ path: 'screenshots/'+filename+'.png' });
-  browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: 'screenshots/'+filename+'.png' });
+    browser.close();
 }
 
 letsCrawl();
